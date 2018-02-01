9d84edc4ec63445de74bfc1379da55c3b86d2834
Zreast <zreast@hotmail.com>
2018-02-01 20:22:05 +0700

    vuetify in da house!

--- bb/src/main.js
+++ bb/src/main.js
@@ -3,7 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from '../node_modules/vuetify'
+import('../node_modules/vuetify/dist/vuetify.min.css')
 
+Vue.use(Vuetify)
+// import Buefy from '../node_modules/buefy/lib'
+// import '../node_modules/buefy/lib/buefy.css'
+
+// Vue.use(Buefy)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
