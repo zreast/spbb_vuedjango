ac10abf5608cc78b8fba721053b984509c2fa68c
Zreast <zreast@hotmail.com>
2018-02-13 12:08:45 +0700

    test api page

---
+++ src/components/test_api.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-app light id="inspire">
+    <v-container>
+  		{{variable}}
+      <v-btn color="primary">Test API</v-btn>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+	/* eslint-disable */
+
+  export default {
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+
+<style>
+</style>
