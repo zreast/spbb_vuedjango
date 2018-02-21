6701b688e3d0828a8cc3a98078b7e596cadf458d
Zreast <zreast@hotmail.com>
2018-02-22 02:44:52 +0700

    cut off unused components

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import store from '../vuex/store'
 import VueResource from 'vue-resource'
 
 const LandingPage = () => import('@/components/')
-const LoginPage = () => import('@/components/login.vue')
 const Request = () => import('@/components/request.vue')
 const TestAPI = () => import('@/components/test_api.vue')
 
@@ -28,28 +26,6 @@ var router = new Router({
       path: '/test_api',
       name: 'Test API',
       component: TestAPI
-    },
-    { path: '/login',
-      name: 'LoginPage',
-      component: LoginPage,
-      beforeEnter: (to, from, next) => {
-        if (localStorage.getItem('auth')) {
-          next('/logout')
-        } else {
-          next()
-        }
-      }
-    },
-    { path: '/logout',
-      beforeEnter: (to, from, next) => {
-        store.commit('Logout')
-        Vue.http.get('/api/logout/').then(response => {
-          window.location.href = '/login'
-        }, response => {
-          console.log(response)
-          window.location.href = '/'
-        })
-      }
     }
   ]
 })
